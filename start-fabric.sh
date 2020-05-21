b77d132b2f22df248ca69b4240480904356295fc
Antoine Toulme <antoine@lunar-ocean.com>
2020-05-20 23:10:24 -0700

    support for helm3

--- start-fabric.sh
+++ start-fabric.sh
@@ -1,13 +1,27 @@
-curl -sSL https://bit.ly/2ysbOFE | bash -s -- 1.4.3
-export PATH=$PATH:$PWD/../fabric-samples/bin
+#!/bin/bash
+set +ex
+curl -sSL http://bit.ly/2ysbOFE | bash -s -- 1.4.3
+export PATH=$PATH:$PWD/fabric-samples/bin
+
 cd fabric-kube
 ./init.sh samples/splunk-fabric samples/chaincode
 helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
 helm dependency update ./hlf-kube/
-helm install hlf-kube --name hlf-kube -f samples/splunk-fabric/network.yaml -f samples/splunk-fabric/crypto-config.yaml --set peer.launchPods=false --set orderer.launchPods=false
+helm install hlf-kube hlf-kube -f samples/splunk-fabric/network.yaml -f samples/splunk-fabric/crypto-config.yaml --set peer.launchPods=false --set orderer.launchPods=false
 ./collect_host_aliases.sh samples/splunk-fabric/
 helm upgrade hlf-kube ./hlf-kube -f samples/splunk-fabric/network.yaml -f samples/splunk-fabric/crypto-config.yaml -f samples/splunk-fabric/hostAliases.yaml
 cd ../
-helm install -n fabric-logger -f fabric-logger-values.yaml -f fabric-kube/samples/splunk-fabric/hostAliases.yaml ./fabric-logger 
-sleep 15
+helm install fabric-logger -f fabric-logger-values.yaml -f fabric-kube/samples/splunk-fabric/hostAliases.yaml ./fabric-logger
+counter=0
+while [[ $(kubectl get pods | grep hlf-cli | grep Running | wc -l) -eq 0 ]];
+do
+  echo "Waiting 10s for CLI container to come up"
+  sleep 10
+  ((counter++))
+  if [[ $counter -ge 20 ]]; then
+    echo "CLI container didn't come up"
+    exit 1
+  fi
+done
+
 kubectl exec hlf-cli -- bash hlf-scripts/channel-setup.sh
