bf3adafc99d2c0845ae7f94d357bec34d3cb59db
Antoine Toulme <antoine@lunar-ocean.com>
2020-07-15 11:05:39 -0700

    Add frontend application

---
+++ frontend/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Builds and deploys the frontend application to minikube
+
+cp -R ../fabric-kube/hlf-kube/crypto-config artifacts/
+docker build -t frontend .
+helm upgrade --install frontend chart/frontend
