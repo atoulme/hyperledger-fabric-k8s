08becc619ab0a76525a513738dd35e9f63eb3acb
Antoine Toulme <antoine@lunar-ocean.com>
2020-07-15 14:53:36 -0700

    enable CA TLS

--- frontend/config.js
+++ frontend/config.js
@@ -5,7 +5,7 @@ var hfc = require('fabric-client');
 // indicate to the application where the setup file is located so it able
 // to have the hfc load it to initalize the fabric client instance
 hfc.setConfigSetting('network-connection-profile-path',path.join(__dirname, 'artifacts', 'network-config.yaml'));
-    hfc.setConfigSetting('TradeFloor-connection-profile-path',path.join(__dirname, 'artifacts', 'tradefloor.yaml'));
+hfc.setConfigSetting('TradeFloor-connection-profile-path',path.join(__dirname, 'artifacts', 'tradefloor.yaml'));
 hfc.setConfigSetting('Acme-connection-profile-path',path.join(__dirname, 'artifacts', 'acme.yaml'));
 // some other settings the application might need to know
 hfc.addConfigFile(path.join(__dirname, 'config.json'));
