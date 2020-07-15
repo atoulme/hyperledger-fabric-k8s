cfd29d3aa07928f7ea0bd334d526b9426b232b50
Antoine Toulme <antoine@lunar-ocean.com>
2020-07-15 11:48:31 -0700

    uninstall release before redeploying

--- frontend/build.sh
+++ frontend/build.sh
@@ -2,6 +2,7 @@
 set -e
 # Builds and deploys the frontend application to minikube
 
+helm uninstall frontend
 cp -R ../fabric-kube/hlf-kube/crypto-config artifacts/
 docker build -t frontend .
 helm upgrade --install frontend chart/frontend
