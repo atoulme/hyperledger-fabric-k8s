4e6eaef7825d69868686ae447574f4c94593be09
Antoine Toulme <antoine@lunar-ocean.com>
2020-07-16 16:49:14 -0700

    Use a version not a complete image ID

--- frontend/build.sh
+++ frontend/build.sh
@@ -3,5 +3,6 @@ set -e
 # Builds and deploys the frontend application to minikube
 
 cp -R ../fabric-kube/hlf-kube/crypto-config artifacts/
-imageTag=$(docker build -t frontend . | grep "Successfully built " | awk '{print $3}')
-helm upgrade --install --set image.tag=$imageTag frontend chart/frontend
+version=`date +%s`
+docker build -t frontend:$version .
+helm upgrade --install --set image.tag=frontend:$version frontend chart/frontend
