7560a28ee5bc90e9bb5fce151164dfb236439852
Antoine Toulme <antoine@lunar-ocean.com>
2020-07-16 18:52:48 -0700

    Add signalfx service

--- start-signalfx.sh
+++ start-signalfx.sh
@@ -2,3 +2,4 @@
 helm repo add signalfx https://dl.signalfx.com/helm-repo
 helm repo update
 helm upgrade --install signalfx-agent -f signalfx/values.yaml  signalfx/signalfx-agent
+kubectl apply -f signalfx/service.yaml
